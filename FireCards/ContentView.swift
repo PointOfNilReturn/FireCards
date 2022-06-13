fee1a02b3b28fab606b7a4be9eaeb36a18ec1c26
Chris Brown <christopheraaronbrown@gmail.com>
2022-06-13 14:13:07 -0500

    Initial Commit

---
+++ FireCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireCards
+//
+//  Created by Chris Brown on 6/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FireCards/FireCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireCardsApp.swift
+//  FireCards
+//
+//  Created by Chris Brown on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct FireCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
